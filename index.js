d12cb6eee81267c8736f354b5c1b152b21b16a97
Zhavgarat <67911288+Zhavgarat@users.noreply.github.com>
2021-01-03 22:41:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import puppeteer from 'puppeteer';
+import express from 'express'
+import puppeteer from 'puppeteer'
 
-import createApp from './app.js';
+import createApp from './app.js'
 
-const app = createApp(express, puppeteer);
+const app = createApp(express, puppeteer)
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000)
